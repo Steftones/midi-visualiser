4a21ae509414d0ef6ca5e7f833c3cdb16867c082
steftones <stefansokolowski99@gmail.com>
2021-07-26 16:15:41 +0100

    midi functionality, shape classes

---
+++ src/client/base-shapes.ts
@@ -0,0 +1,119 @@
+import * as THREE from 'three'
+import { MeshNormalMaterialParameters } from 'three'
+import { scene, randomRange, nitro } from './client'
+
+class Shape {
+  shape: THREE.Mesh; geometry: any; material: THREE.MeshNormalMaterial;
+  spin: boolean; spinAmount: number; randomSpin: boolean; decreaseScale: boolean; isRotating: boolean;
+  rotateDirectionLeft: boolean; randomValues: number;
+
+  constructor() {
+    this.geometry = new THREE.BoxGeometry()
+    this.material = new THREE.MeshNormalMaterial({
+      transparent: true,
+      wireframe: false,
+      opacity: 0.5
+    })
+    this.shape = new THREE.Mesh(this.geometry, this.material)
+    this.spin = false
+    this.spinAmount = 0.01
+    this.randomSpin = false
+    this.decreaseScale = true
+    this.isRotating = true
+    this.rotateDirectionLeft = true
+    this.randomValues = randomRange(0,0.1)
+  }
+
+  stopSpin(){
+    this.spin = false
+  }
+
+  spinShape(amount: number = 0.03){
+    this.spin = true
+    this.spinAmount = amount
+  }
+
+  toggleRotateDirection(){
+    this.rotateDirectionLeft = !this.rotateDirectionLeft
+  }
+
+  toggleRandomSpin(){
+    this.randomSpin = !this.randomSpin
+  }
+
+  setScale(x: number = 1, y: number = 1, z: number = 1){
+    this.shape.scale.x = x
+    this.shape.scale.y = y
+    this.shape.scale.z = z
+  }
+
+  // scale increase / decrease
+  increaseScale(amount: number = (Math.random() * (3 - 1.75) + 1.75), axis: string = 'x', decreaseScale: boolean = true){
+    if (axis === 'x' || axis === 'xy') this.shape.scale.x = amount
+    if (axis === 'y' || axis === 'xy') this.shape.scale.y = amount
+    this.decreaseScale = decreaseScale
+  }
+
+  updateMaterial(input: MeshNormalMaterialParameters){
+    this.material.setValues(input)
+  }
+
+  update(){
+    // maybe spinning and rotating are the same thing?
+    if (this.spin) this.shape.rotation.x += this.spinAmount
+    if (this.decreaseScale){
+      if (this.shape.scale.x > 1) this.shape.scale.x -= 0.05
+      if (this.shape.scale.y > 1) this.shape.scale.y -= 0.05
+    }
+    if (this.isRotating){
+      this.shape.rotation.x += this.rotateDirectionLeft ? 0.01 : -0.01
+      this.shape.rotation.y += this.rotateDirectionLeft ? 0.01 : -0.01
+    }
+    if (this.randomSpin){
+      this.shape.rotation.z += (this.rotateDirectionLeft ? this.randomValues : -Math.abs(this.randomValues)) + nitro
+      this.shape.rotation.x += 0
+      this.shape.rotation.y += 0
+      // this.rotateDirectionLeft ? 0.1 : -0.1
+    }
+  }
+}
+
+export class Cube extends Shape {
+  constructor() {
+    super()
+    scene.add(this.shape)
+  }
+  updateGeometry(width: number = 1, height: number = 1, depth: number = 1){
+    const newGeometry = new THREE.BoxGeometry(width, height, depth)
+    this.shape.geometry.dispose()
+    this.shape.geometry = newGeometry
+  }
+}
+
+export class Cone extends Shape {
+  constructor() {
+    super()
+    this.geometry = new THREE.ConeBufferGeometry()
+    this.shape = new THREE.Mesh(this.geometry, this.material)
+    scene.add(this.shape)
+  }
+  updateGeometry(width: number = 1, height: number = 1, depth: number = 1){
+    const newGeometry = new THREE.ConeBufferGeometry(width, height, depth)
+    this.shape.geometry.dispose()
+    this.shape.geometry = newGeometry
+  }
+}
+
+export class Sphere extends Shape {
+  constructor() {
+    super()
+    this.geometry = new THREE.SphereGeometry()
+    this.shape = new THREE.Mesh(this.geometry, this.material)
+    scene.add(this.shape)
+  }
+  updateGeometry(width: number = 1, height: number = 1, depth: number = 1){
+    const newGeometry = new THREE.SphereGeometry(width, height, depth)
+    this.shape.geometry.dispose()
+    this.shape.geometry = newGeometry
+  }
+}
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,17 +1,22 @@
-// install @types/three to get ts types from three
 import * as THREE from 'three'
-// importing extra modules
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-// a lot of modules in THREE won't have ts types. You have to always tell your ts config where the location of your new type definition should be when it finds that path in your code
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 import * as Tone from 'tone'
 
+// base shapes to render
+import { Sphere, Cone, Cube } from './base-shapes'
+
 // speeds up rotation
-let nitro = 0
+export let nitro = 0
 
-const scene = new THREE.Scene()
+export function randomRange(min: number, max: number): number {
+  return Math.random() * (max - min) + min
+}
 
+export const scene = new THREE.Scene()
+
+// initial setup
 const camera = new THREE.PerspectiveCamera(
   75, // FOV
   window.innerWidth / window.innerHeight,
@@ -19,48 +24,17 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 camera.position.z = 2
-
-// fastest and default renderer you'll see on most THREE projects
-// you need to set the size of the renderer
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-// renderer draws its output, dynamically adding it to the document body. We need this to draw the canvas. You can hardcode your canvas by creating <canvas> and passing it into THREE.WebGLRenderer()
 document.body.appendChild(renderer.domElement)
 
 // allows you to control the cube with the mouse
 new OrbitControls(camera, renderer.domElement)
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-  color: 'black',
-  wireframe: true
-})
 
 // adding a background colour
 scene.background = new THREE.Color('orange')
 
-// adding a cube
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-// adding some sort of shape
-const shapeGeometrySettings = {
-  width: 1,
-  height: 1,
-  depth: 100
-}
-const shapeGeometry = new THREE.SphereGeometry(
-  shapeGeometrySettings.width,
-  shapeGeometrySettings.height,
-  shapeGeometrySettings.depth
-)
-const shapeMaterial = new THREE.MeshNormalMaterial()
-shapeMaterial.wireframe = false
-shapeMaterial.flatShading = true
-const shape = new THREE.Mesh(shapeGeometry, shapeMaterial)
-shapeGeometry.scale(0.5,0.5,0.5)
-scene.add(shape)
-
-// resizes 3d view on the canvas
+// resize 3d view on the canvas
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
@@ -69,106 +43,35 @@ function onWindowResize() {
   render()
 }
 
-// need to update in the animate function so you can see it updating all the time
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-// ? ----------- adding GUI -----------
-const gui = new GUI()
+// ? ----- shape declerations ----
 
-// adding folders to organise within the GUI
-const cubeFolder = gui.addFolder('Cube')
-// cubeFolder.open()
+const newMiddle = new Sphere()
+newMiddle.updateGeometry(0.5, 0.5, 1)
 
-function generateShapeGeometry(
-  width = shapeGeometrySettings.width,
-  height = shapeGeometrySettings.height,
-  depth = shapeGeometrySettings.depth){
-  const newGeometry = new THREE.SphereGeometry(
-    width,
-    height,
-    depth
-  )
-  shape.geometry.dispose()
-  shape.geometry = newGeometry
-}
+const newMiddles = new Cube()
+newMiddles.toggleRotateDirection()
 
-const shapeGeometryFolder = gui.addFolder('Shape geometry')
-shapeGeometryFolder.add(shapeGeometrySettings, 'width', 0, 0.5, 0.1).onChange(generateShapeGeometry)
-shapeGeometryFolder.add(shapeGeometrySettings, 'height', 0, 0.5, 0.1).onChange(generateShapeGeometry)
-shapeGeometryFolder.add(shapeGeometrySettings, 'depth', 0, 100, 1).onChange(generateShapeGeometry)
-// shapeGeometryFolder.open()
+const newCube = new Cube()
+newCube.updateMaterial({ opacity: 0.4 })
 
-const cubeRotationFolder = cubeFolder.addFolder('Rotation')
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-// cubeRotationFolder.open()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 10)
-
-const cubePositionFolder = gui.addFolder('Cube Position')
-cubePositionFolder.add(cube.position, 'x', -10, 10, 0.1)
-cubePositionFolder.add(cube.position, 'y', -10, 10, 0.1)
-cubePositionFolder.add(cube.position, 'z', -10, 10, 0.1)
-
-const cubeScaleFolder = gui.addFolder('Cube Scale')
-cubeScaleFolder.add(cube.scale, 'x', -10, 10, 0.1)
-cubeScaleFolder.add(cube.scale, 'y', -10, 10, 0.1)
-cubeScaleFolder.add(cube.scale, 'z', -10, 10, 0.1)
-
-const options = {
-  side: {
-    'FrontSide': THREE.FrontSide,
-    'BackSide': THREE.BackSide,
-    'DoubleSide': THREE.DoubleSide
-  }
-}
-
-const materialFolder = gui.addFolder('Internal shape material')
-materialFolder.add(shapeMaterial, 'transparent')
-materialFolder.add(shapeMaterial, 'opacity', 0, 1, 0.01)
-materialFolder.add(shapeMaterial, 'depthTest')
-materialFolder.add(shapeMaterial, 'depthWrite')
-materialFolder.add(shapeMaterial, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
-materialFolder.add(shapeMaterial, 'side', options.side).onChange(() => updateMaterial())
-materialFolder.open()
-
-// ? ---------------------------------
-
-function updateMaterial(){
-  shapeMaterial.side = Number(shapeMaterial.side)
-  // next time rendered
-  shapeMaterial.needsUpdate = true
-}
-
-function randomRange(min: number, max: number): number {
-  return Math.random() * (max - min) + min
-}
+// ? ----- for animation -----
 
 function animate() {
-  // window.requestAnimationFrame is a method that tells the browser that you want to perform an animation. Requests that the browser calls a function to update an animation before the next repeat. Takes a callback as an argument to be invoked before the next repaint.
-  // will run faster on different browsers
-  // animate needs to be called
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-
-  // decrease cube scale
-  if (cube.scale.x > 1) cube.scale.x -= 0.05
-
-  shape.rotation.z += randomRange(0,0.1) + nitro
-  shape.rotation.x += randomRange(0,0.1)
-  shape.rotation.y += 0.1
-
-  shapeGeometry.scale(1, 1, 1)
+  // rendering all objects
+  newMiddle.update()
+  newMiddles.update()
+  newCube.update()
 
   stats.update()
-  // can call once if you only need to render it once, e.g. out of animate
   render()
 }
+
+animate()
 
 function render() {
   renderer.render(scene, camera)
@@ -176,17 +79,22 @@ function render() {
 
 function kickAnimations(){
   console.log('kicked')
-  cube.scale.x = (Math.random() * (3 - 1.5) + 1.5)
+  newCube.increaseScale()
+  newCube.stopSpin()
 }
 
 let snareNitroToggle = false
 function snareAnimations(){
+  console.log('snared')
   snareNitroToggle = !snareNitroToggle
-  snareNitroToggle ? (Math.random() < 0.5 ? nitro = 20 : nitro = 0) : nitro = 0
-  Math.round(Math.random()) ? generateShapeGeometry(0.5) : generateShapeGeometry(0.7)
+  snareNitroToggle ? (Math.random() < 0.5 ? nitro = 0.3 : nitro = 0) : nitro = 0
+  Math.round(Math.random()) ? newMiddle.updateGeometry(0.5, 0.7, 1) : newMiddle.updateGeometry(0.6, 0.7, 1)
+  newMiddle.setScale(randomRange(3,5), randomRange(0,3))
 }
 
 let transportOn = true
+
+// ? ----- temporary measure for demonstration -----
 document.addEventListener('keydown', (event) => {
   console.log(`pressed: ${event.key}`)
   if (event.key === 's'){
@@ -197,39 +105,45 @@ document.addEventListener('keydown', (event) => {
     transportOn = !transportOn
     transportOn ? Tone.Transport.stop() : Tone.Transport.start()
   }
+  if (event.key === 'y'){
+    snareAnimations()
+  }
+  if (event.key === 'u'){
+    kickAnimations()
+  }
 })
 
-animate()
 
-// ? ----- Tone experiment -----
 
+// ? ----- temporary/Demo with ToneJs -----
 const currentSong = {
   kick: [
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, false,
-    true, false, false, true, false, false, false, true
+    true, false, false, false, false, false, false, false,
+    false, false, true, false, false, false, false, false,
+    true, false, false, true, false, false, true, false,
+    false, false, false, false, false, false, true, false
   ],
   snare: [
-    false, false, true, false, false, false, true, false,
-    false, false, true, false, false, false, true, false,
-    false, false, true, false, false, false, true, false,
-    false, false, true, false, false, false, true, false
+    false, false, false, false, true, false, false, false,
+    false, false, false, false, true, false, false, false,
+    false, false, false, false, false, false, false, false,
+    false, false, false, false, true, false, false, false
   ]
 }
 
-Tone.Transport.bpm.value = 170
-// Tone.Transport.start()
+Tone.Transport.bpm.value = 240
 Tone.Transport.scheduleRepeat((time) => { 
   repeat(time)
 }, '8n')
 
-// Kick
+// generating a note
 const kick = new Tone.Synth().toDestination()
+const verb = new Tone.Reverb().toDestination()
+kick.connect(verb)
 kick.volume.value = -5
 
 let index = 0
-const repeat = (time: any) => {
+const repeat = (time: any): void => {
   const position = index % currentSong.kick.length
   const synthNote1 = currentSong.kick[position]
   const synthNote2 = currentSong.snare[position]
@@ -244,33 +158,51 @@ const repeat = (time: any) => {
   index++
 }
 
-// ? midi experiment
-// npm install @types/webmidi
+// accessing the web midi API
 window.navigator.requestMIDIAccess()
   .then(access => {
     console.log(access)
-    // getting device
     const device: any = access.inputs.values().next()
     console.log(
       'Device found! - Output port [type:\'' + device.value.type + '\'] id:\'' + device.value.id +
-      '\' manufacturer:\'' + device.value.manufacturer + '\' name:\'' + device.value.name +
-      '\' version:\'' + device.value.version + '\''
+        '\' manufacturer:\'' + device.value.manufacturer + '\' name:\'' + device.value.name +
+        '\' version:\'' + device.value.version + '\''
     )
-    // problem - it will only work on one midi device that's plugged in...
     device.value.onmidimessage = onMidiMessage
   })
-  .catch(() => midiAccessFail())
+  .catch(() => console.log('No midi found!'))
 
-let midiNoteOn = false
-function onMidiMessage(message: any){
-  midiNoteOn = !midiNoteOn
-  if (midiNoteOn){
-    Math.random() < 0.3 ? nitro = 2 : nitro = 0
-    cube.scale.x = (Math.random() * (3 - 1.5) + 1.5)
-    console.log(message)
+// reads midi messages to be played
+function onMidiMessage(message: any ): void {
+  const [command, note, value] = message.data
+  switch (command) {
+    case 144: // Note on
+      switch (value){
+        case 60:
+          kickAnimations()
+          break
+        case 62:
+          snareAnimations()
+          break
+        case 64:
+          newCube.spinShape()
+          break
+        case 65:
+          newCube.spinShape(0.1)
+          break
+        case 67:
+          // some functionality
+          break
+        case 69:
+          // some functionality
+          break
+      }
+      console.log(`Note: '${note}'. With value: ${value}`)
+      break
+    default: // Note off
+      console.log(`Note OFF: '${note}'. With value: '${value}`)
+      break
   }
 }
 
-function midiAccessFail(){
-  console.log('no midi!')
-}
+
